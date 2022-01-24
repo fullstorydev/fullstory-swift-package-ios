d1a988d9fb31d7af9cc9dc626198cca0e48d0789
Prashanth Sadasivan <prashanth@fullstory.com>
2022-01-24 17:06:46 -0500

    update package for 1.23.0 release

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FullStory",
-            url: "https://ios-releases.fullstory.com/fullstory-1.19.0-xcframework.zip",
-            checksum: "ff00324bf60c4be379e61e8af590dce962f13fffe507dd9164bfc448e8969293"
+            url: "https://ios-releases.fullstory.com/fullstory-1.23.0-xcframework.zip",
+            checksum: "c44b4ba8942c0135e11aed7245ad7c53fd66cb5fd1a331704c9527f1751af64f"
         ),
     ]
 )
